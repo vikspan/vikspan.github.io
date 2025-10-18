d568eef9d439118dfded1d26279f299b8bd3555a
vikspan <viktorija.span@gmail.com>
2025-10-18 12:32:21 +0200

    Edit footer: adapt attribution line for proper credit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,18 +3,15 @@ function Footer() {
   return (
     <footer className="container mx-auto py-1 fixed bottom-0 md:left-20 bg-white dark:bg-dark-mode">
       <p className="text-xs text-center text-dark-content dark:text-light-content w-full">
-        Designed and Coded by{" "}
+        Adapted by Viktorija Špan from a template by {" "}
         <a
           className="font-medium"
-          href="https://pavanmg.in"
+          href="https://github.com/pavanmg007/react-developer-portfolio-template"
           target="_blank"
           rel="noreferrer noopener"
         >
           Pavan MG
         </a>{" "}
-        with
-        <span className="text-gradient font-medium"> Love</span> &
-        <span className="text-gradient font-medium"> Coffee</span>
       </p>
     </footer>
   );
