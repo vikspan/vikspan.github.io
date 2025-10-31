ec84f817d16f7371e8223caeda63fbb1e8267826
vikspan <viktorija.span@gmail.com>
2025-10-31 16:58:24 +0100

    fix footer centering

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,19 +1,19 @@
 import React from "react";
 function Footer() {
   return (
-    <footer className="container mx-auto py-1 fixed bottom-0 md:left-20 bg-white dark:bg-dark-mode">
-      <p className="text-xs text-center text-dark-content dark:text-light-content w-full">
-        Adapted by Viktorija Špan from a template by {" "}
-        <a
-          className="font-medium"
-          href="https://github.com/pavanmg007/react-developer-portfolio-template"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Pavan MG
-        </a>{" "}
-      </p>
-    </footer>
+<footer className="fixed bottom-0 left-0 w-full flex justify-center">
+    <p className="text-xs text-center text-dark-content px-2 bg-white rounded-t-md">
+      Adapted by Viktorija Špan from a template by{" "}
+      <a
+        className="font-medium"
+        href="https://github.com/pavanmg007/react-developer-portfolio-template"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Pavan MG
+      </a>
+    </p>
+</footer>
   );
 }
 export default Footer;
